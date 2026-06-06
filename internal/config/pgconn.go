9fc26e51dfa9c9f1b6e9f27d6cfaf5cc5b89033f
anggavb <anggavb8@gmail.com>
2026-06-06 17:43:15 +0700

    refactor(config): simplify database connection logic by using DB_URL

--- internal/config/pgconn.go
+++ internal/config/pgconn.go
@@ -2,20 +2,12 @@ package config
 
 import (
 	"context"
-	"fmt"
 	"os"
 
 	"github.com/jackc/pgx/v5/pgxpool"
 )
 
 func ConnectPsql() (*pgxpool.Pool, error) {
-	values := make([]any, 0, 5)
-	values = append(values, os.Getenv("DB_USER"))
-	values = append(values, os.Getenv("DB_PASS"))
-	values = append(values, os.Getenv("DB_HOST"))
-	values = append(values, os.Getenv("DB_PORT"))
-	values = append(values, os.Getenv("DB_NAME"))
-	connStr := fmt.Sprintf("postgresql://%s:%s@%s:%s/%s?sslmode=disable", values...)
-	pgc, _ := pgxpool.New(context.Background(), connStr)
+	pgc, _ := pgxpool.New(context.Background(), os.Getenv("DB_URL"))
 	return pgc, pgc.Ping(context.Background())
 }
